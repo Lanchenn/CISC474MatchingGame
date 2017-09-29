8775f3978ebe05cac9e61509478d2f578b654e06
Estella <Telle@Estellas-MacBook-Pro.local>
2017-09-29 14:20:05 -0400

    flipping works! and images changed to display blank when face-down, and specific images when face-up.

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
 
-  $('.clickBox').click(function() {
+  $('.card').click(function() {
     var parentid = $(this).parent().attr('id');
     var cardSpot = document.getElementById(parentid).children[0];
-    var cardIMG = cardSpot.children[0];
-    var backIMG = cardSpot.children[1];
-    if ($(cardIMG).is(':visible')) {
-      $(cardIMG).hide();
-      cardSpot.style.background = 'red';
-      $(backIMG).show();
+  //  var cardSpot = $this.attr('id') <!-- using this line instead of the two above causes nothing to happen when card is clilcked-->
+
+    //var cardIMG = cardSpot.children[0];
+    //var backIMG = cardSpot.children[1];
+    // alert($(parentid).attr('id'));
+    //
+    // alert($(cardSpot).attr('id'));
+    //alert($(cardIMG).attr('id'));
+    //alert($(backIMG).attr('id'));
+
+    // alert($(this).attr('id'));
+
+
+
+    if ($(this).hasClass('flipped')) {
+      // $(cardIMG).hide();
+      // cardSpot.style.background = 'red';
+      // $(backIMG).show();
+      $(this).removeClass('flipped');
+
     }
     else {
-      $(cardIMG).show();
-      cardSpot.style.background = 'transparent';
-      $(backIMG).hide();
+      // $(cardIMG).show();
+      // cardSpot.style.background = 'transparent';
+      // $(backIMG).hide();
+      $(this).addClass('flipped');
+      $(this).addClass('flipper');
     }
   })
 });
