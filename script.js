2e946a463eaa82030c382437d8f09e1767ccd9c9
wliapple <esme@udel.edu>
2017-10-03 14:18:54 -0400

    test music and flip sound

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   $('.card').click(function() {
+    woosh.play();
     var parentid = $(this).parent().attr('id');
     var cardSpot = document.getElementById(parentid).children[0];
   //  var cardSpot = $this.attr('id') <!-- using this line instead of the two above causes nothing to happen when card is clilcked-->
