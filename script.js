a4257fde0a19c855204867ea014383bef435a5b2
Estella <Telle@Estellas-MacBook-Pro.local>
2017-10-06 07:39:08 -0400

    Commit to merge.

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+//separate this into function "flip" and the rest.
+
 $(document).ready(function() {
 
+  var i = 0;
+  var card1ID;
+  var card2ID;
+  var card1SRC;
+  var card2SRC;
+
   $('.card').click(function() {
+    i++;
+
+    //do this right away in case there is a delay. don't want the user to be able to use the delay to click a 3rd card.
+    if( i%2 == 0 && i!=0){
+      //disable all cards
+      $("#gameBoard").addClass("disabledbutton");
+    }
     var parentid = $(this).parent().attr('id');
     var cardSpot = document.getElementById(parentid).children[0];
   //  var cardSpot = $this.attr('id') <!-- using this line instead of the two above causes nothing to happen when card is clilcked-->
 
-    if ($(this).hasClass('flipped')) {
-      $(this).removeClass('flipped');
+
+    if((i%2)==0 && i!=0){
+      //check if this card matches the other on the board that has class flipped
+      card2ID = $(this).parent().attr('id');
+      // $(this).parent().attr('id');
+      card2SRC = $(this).children("div.back").children("img").attr("src");
+      // alert($(this).children("div.back").children("img").attr("src"));
+      alert(card2SRC);
+
+      if(card1SRC == card2SRC){
+        alert("yes!");
+        //reenable game board
+        $("#gameBoard").removeClass("disabledbutton");
+        //disable these two cards
+      }
+      else{
+        alert("nope");
+        //flip cards back over
+        //reenable the game board
+        $("#gameBoard").removeClass("disabledbutton");
+      }
     }
-    else {
-      $(this).addClass('flipped');
-      $(this).addClass('flipper');
+    else{
+      card1ID = $(this).parent().attr('id');
+      $(this).parent().attr('id');
+      card1SRC = $(this).children("div.back").children("img").attr("src");
+      // alert($(this).children("div.back").children("img").attr("src"));
+      alert(card1SRC);
     }
+alert("here5");
   })
 });
